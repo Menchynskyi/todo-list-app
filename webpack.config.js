79395bd8c72c753c874d4d5f8f4888f0fa0be03d
Menchynskyi <the.mngs@yahoo.com>
2019-11-02 12:23:23 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  mode: "development",
+  entry: "./src",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            }
+          }
+        ],
+      },
+    ]
+  }
+};
